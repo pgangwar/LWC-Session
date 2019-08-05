afad69f8287c586f82de032c3c1ef1aa25d1d22b
pgangwar <pgangwar@hotmail.com>
2019-08-05 17:56:54 -0400

    Commit

---
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+    meetingRoomInfo =[
+        {roomName:'A-01', roomCapacity:'11'},
+        {roomName:'A-02', roomCapacity:'12'},
+        {roomName:'A-03', roomCapacity:'09'},
+        {roomName:'B-01', roomCapacity:'09'},
+        {roomName:'B-02', roomCapacity:'12'},
+        {roomName:'B-03', roomCapacity:'06'},
+        {roomName:'C-01', roomCapacity:'04'}
+    ];
+}
